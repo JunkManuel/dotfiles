b2ba9c00d6ef34f21fb1b45ce800c7204ca148a9
KiraManolo <21977499+JunkManuel@users.noreply.github.com>
2024-01-21 23:03:54 +0100

    now install does something

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env sh
 #
 # Usamo stow para symlinkear
+
+conf_dirs=( kitty neovim pywal scripts waybar WebCord wlogout wofi xdg-user-dirs zsh )
+
+for dir in conf_dirs;
+do
+	stow -t $HOME dir
+done
