c5bc5e6d4175d3a49d8cb1ed8c2d3f1b492aeec6
KiraManolo <21977499+JunkManuel@users.noreply.github.com>
2024-02-01 21:01:15 +0100

    docker lsp added

--- neovim/.config/nvim/lua/lsp.lua
+++ neovim/.config/nvim/lua/lsp.lua
@@ -1,8 +1,31 @@
 -- Setup language servers.
 local lspconfig = require('lspconfig')
+
 -- Python language server
 lspconfig.jedi_language_server.setup {}
+
 -- Lua language server
 lspconfig.lua_ls.setup {}
+
 -- VimScript language server
 lspconfig.vimls.setup {}
+
+-- dockerfiles language server
+lspconfig.dockerls.setup {}
+lspconfig.docker_compose_language_service.setup {
+	filetypes = { "yaml.docker-compose" },
+}
+
+-- Enable docker compose lsp with filename
+function docker_fix()
+    local filename = vim.fn.expand("%:t")
+
+    if filename == "docker-compose.yaml" then
+        vim.bo.filetype = "yaml.docker-compose"
+        print("matched!")
+    else
+        print(filename)
+    end
+end
+
+vim.cmd[[au BufRead * lua docker_fix()]]
