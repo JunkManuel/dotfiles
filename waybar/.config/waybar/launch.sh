55cbbc53414ca1c182e45ca5507bab423a8c9807
KiraManolo.LENOVO <21977499+JunkManuel@users.noreply.github.com>
2024-02-22 15:44:32 +0100

    waybar css refactor + style changes (linear gradients, we love em)

--- waybar/.config/waybar/launch.sh
+++ waybar/.config/waybar/launch.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-pkill -o waybar
-/usr/bin/waybar -c ~/.config/waybar/config 
+ps -p $(pgrep waybar) >& /dev/null
+
+if [ $? -eq 0 ]; then
+		# echo "hay proceso"	
+		sh -c "~/.local/bin/reload-bar"
+else
+		# echo "nn hay proceso"
+		sh -c "/usr/bin/waybar -c ~/.config/waybar/config.jsonc >& $(mktemp --suffix=.waybar.log)" &
+fi
