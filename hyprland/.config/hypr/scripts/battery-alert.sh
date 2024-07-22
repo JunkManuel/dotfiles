165530817ad50bdf6613fb2585b105bf50449a9b
KiraMantler.T480 <KiraMantler.21977499+JunkManuel@users.noreply.github.com>
2024-07-22 20:04:24 +0200

    mpv now works on ranger

--- hyprland/.config/hypr/scripts/battery-alert.sh
+++ hyprland/.config/hypr/scripts/battery-alert.sh
@@ -4,11 +4,10 @@ while true; do
 
     bat=$(cat /sys/class/power_supply/BAT1/capacity)
 
-    if [ $bat -le 10 ]; then
+    if [ $bat -le 15 ]; then
         notify-send --urgency=critical "Battery Low" "Level: ${bat}"
         sleep 2000
     else
-        sleep 120
+        sleep 30
     fi
-
 done
